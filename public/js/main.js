7f506eed53b4d45b028b0bc5497be028bd747b70
philManWithBeard <48831325+philManWithBeard@users.noreply.github.com>
2020-07-29 21:25:07 +0100

    meh

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,20 @@
 'use strict'
 
+
 /**
  * Obtains parameters from the hash of the URL
  * @return Object
  */
 function getHashParams() {
-  var hashParams = {};
-  var e, r = /([^&;=]+)=?([^&;]*)/g,
-      q = window.location.hash.substring(1);
-  while ( e = r.exec(q)) {
-     hashParams[e[1]] = decodeURIComponent(e[2]);
+  const hashParams = {}
+  let e
+  let r = /([^&;=]+)=?([^&;]*)/g
+  let q = window.location.hash.substring(1)
+  while (e === r.exec(q)) {
+    hashParams[e[1]] = decodeURIComponent(e[2])
   }
-  console.log(hashParams)
-  return hashParams;
+  console.log(hashParams + "hello")
+  return hashParams
 }
 
 const params = getHashParams()
