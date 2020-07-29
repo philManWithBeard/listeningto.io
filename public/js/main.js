e03f38d11d65eb51250b847f0a0befe0cc29ddc8
philManWithBeard <48831325+philManWithBeard@users.noreply.github.com>
2020-07-29 20:04:18 +0100

    params

--- public/js/main.js
+++ public/js/main.js
@@ -22,7 +22,7 @@ const accessToken = params.access_token
 const error = params.error
 const searchURL = 'https://api.spotify.com/v1/me/top/artists';
 
-console.log(accessToken)
+console.log(params)
 if (accessToken) {
   console.log("access token")
     getSpotifyTopSix()
